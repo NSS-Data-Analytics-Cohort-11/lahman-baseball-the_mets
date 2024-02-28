152b6e614b412b5d6ccb0cb9d360c12a3efa2436
derekbeistad <derekbeistad@gmail.com>
2024-02-27 21:22:26 -0600

    Finished Q4

--- scripts/lahman-baseball-the-mets.sql
+++ scripts/lahman-baseball-the-mets.sql
@@ -29,8 +29,7 @@ WHERE playerid = (SELECT playerid
 -- 			A. 1 game
 
 /* ***** Final Query ***** */
-SELECT p.namefirst,
-		p.namelast,
+SELECT CONCAT(p.namefirst, ' ', p.namelast) AS player_name,
 		p.namegiven,
 		p.height,
 		a.g_all,
@@ -42,8 +41,42 @@ INNER JOIN teams AS t
 ON a.teamid = t.teamid AND a.yearid = t.yearid
 WHERE p.height = (SELECT MIN(height::INTEGER)
 				FROM people)
--- 		A. "Eddie"	"Gaedel"	"Edward Carl"	43	1	"St. Louis Browns"
+-- 		A. "Eddie Gaedel"	"Edward Carl"	43	1	"St. Louis Browns"
 
 -- 3. Find all players in the database who played at Vanderbilt University. Create a list showing each player’s first and last names as well as the total salary they earned in the major leagues. Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?
-SELECT DISTINCT schoolname
+SELECT schoolid
 FROM schools
+WHERE schoolname ILIKE '%vanderbilt%'
+-- 		A. "vandy"
+
+SELECT COUNT(DISTINCT p.playerid)
+FROM people AS p
+INNER JOIN collegeplaying AS c
+ON p.playerid = c.playerid
+WHERE c.schoolid = 'vandy'
+-- 24 different players at Vandy
+
+SELECT CONCAT(p.namefirst, ' ', p.namelast) AS player_name,
+		SUM(COALESCE(s.salary, 0)) AS total_salary
+FROM people AS p
+LEFT JOIN salaries AS s
+ON p.playerid = s.playerid
+INNER JOIN collegeplaying AS c
+ON p.playerid = c.playerid
+WHERE c.schoolid = 'vandy'
+GROUP BY CONCAT(p.namefirst, ' ', p.namelast)
+ORDER BY total_salary DESC;
+-- 		A. "David Price"	245553888
+
+-- 4. Using the fielding table, group players into three groups based on their position: label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery". Determine the number of putouts made by each of these three groups in 2016.
+SELECT CASE WHEN pos = 'OF' THEN 'Outfield'
+			WHEN pos IN ('1B', '2B', '3B', 'SS') THEN 'Infield'
+			WHEN pos IN ('P', 'C') THEN 'Battery'
+			ELSE 'Other' END AS position,
+			SUM(po)
+FROM fielding
+WHERE yearid = '2016'
+GROUP BY position;
+ 		/* A. "Battery"	41424
+ 			  "Infield"	58934
+			  "Outfield"29560 */
