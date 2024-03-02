eaf10254ccafc9037bd4d13427d6dc5c4780a2d3
barbsjean <barbarajbrake@gmail.com>
2024-03-02 10:32:34 -0600

    pushing my work randomly for saving!

--- data/barb_branch_lahman.sql
+++ data/barb_branch_lahman.sql
@@ -9,6 +9,8 @@ FROM teams
 -- MIN YEAR: 1871
 -- MAX YEAR: 2016
 
+************************************************************************************************
+
 -- 2. Find the name and height of the shortest player in the database. How many games did he play in? What is the name of the team for which he played?
 
 select
@@ -29,6 +31,7 @@ USING (teamid, yearid)
 
 WHERE height = (SELECT MIN(height) from people)
 
+************************************************************************************************
 
 -- 3. Find all players in the database who played at Vanderbilt University. Create a list showing each player’s first and last names as well as the total salary they earned in the major leagues. Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?
 
@@ -49,29 +52,7 @@ WHERE playerid IN
 GROUP BY playerid, namefirst, namelast
 ORDER BY total_salary DESC
 
-
-
--- SELECT 
--- 	playerid,
--- 	CONCAT(namefirst, ' ', namelast) AS player_name,
--- 	COALESCE(SUM(salary), 0) AS total_salary
--- from collegeplaying
--- INNER JOIN people
--- USING (playerid)
--- INNER JOIN salaries
--- USING (playerid)
--- where schoolid ILIKE '%Vandy%'
--- GROUP BY playerid, player_name
--- ORDER BY total_salary DESC    -- 245553888
-
--- SELECT SUM(salary)
--- FROM salaries
--- WHERE playerid = 'priceda01'   -- 81851296
-
-
-
-
-
+************************************************************************************************
 
 -- 4. Using the fielding table, group players into three groups based on their position: label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery". Determine the number of putouts made by each of these three groups in 2016.
 
@@ -92,6 +73,7 @@ from fielding
 where yearid = 2016
 group by position 
 
+************************************************************************************************
 
 -- 5. Find the average number of strikeouts per game by decade since 1920. Round the numbers you report to 2 decimal places. Do the same for home runs per game. Do you see any trends?
 
@@ -103,6 +85,8 @@ from teams
 where yearid >= 1920
 group by decade
 order by decade
+
+************************************************************************************************
 
 -- 6. Find the player who had the most success stealing bases in 2016, where __success__ is measured as the percentage of stolen base attempts which are successful. (A stolen base attempt results either in a stolen base or being caught stealing.) Consider only players who attempted _at least_ 20 stolen bases.
 
@@ -122,6 +106,7 @@ WHERE yearid = 2016
 group by yearid, player_name, sb, cs
 -- order by stolen_bases
 
+************************************************************************************************
 
 -- 7. From 1970 – 2016, what is the largest number of wins for a team that did not win the world series? What is the smallest number of wins for a team that did win the world series? Doing this will probably result in an unusually small number of wins for a world series champion – determine why this is the case. Then redo your query, excluding the problem year. How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? What percentage of the time?
 
@@ -137,7 +122,7 @@ where WSWin is not null
 	and WSWin = 'N'
 	and yearid BETWEEN 1970 AND 2016
 order by wins desc;
--- 116 wins
+-- 116 wins, Seattle Mariners
 
 
 -- What is the smallest number of wins for a team that did win the world series?
@@ -152,16 +137,106 @@ where wswin is not null
 	and wswin = 'Y'
 	and yearid BETWEEN 1970 AND 2016
 order by wins;
--- 63 wins
+-- 63 wins, Los Angeles Dodgers
 
 
 -- Doing this will probably result in an unusually small number of wins for a world series champion – determine why this is the case.
--- 1981, there was a strike during this year.
+-- 1981, there was a baseball strike during this year.
 	
 -- Then redo your query, excluding the problem year. How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? What percentage of the time?
 
+WITH teams_1970_2016 AS
 
-working on how to filter a specific date from a range.....
+(
+select
+	yearid as year,
+	name as team,
+	WSWin as world_series_winner,
+	w AS wins,                                                             -- !!! ALMOST DONE, NEED TO FINALIZE THE END OF THE QUESTION !!! --
+	l as losses
+from teams
+where WSWin is not null
+	and WSWin = 'N'
+	and yearid BETWEEN 1970 AND 2016
+	
+UNION ALL
+
+select 
+	yearid as year,
+	name as team,
+	WSWin as world_series_winner,
+	w AS wins,
+	l AS losses
+from teams
+where wswin is not null
+	and wswin = 'Y'
+	and yearid BETWEEN 1970 AND 2016
+)
+
+
+SELECT 
+	year,
+	team,
+	world_series_winner,
+	wins
+FROM teams_1970_2016
+WHERE year <> 1981
+-- 	AND world_series_winner = 'Y'
+ORDER BY year;
+
+************************************************************************************************
+
+-- 8. Using the attendance figures from the homegames table, find the teams and parks which had the top 5 average attendance per game in 2016 (where average attendance is defined as total attendance divided by number of games). Only consider parks where there were at least 10 games played. Report the park name, team name, and average attendance. Repeat for the lowest 5 average attendance.
+
+(
+SELECT 
+	hg.park,
+	t.name,
+	SUM(hg.attendance) / SUM(hg.games) AS avg_attendance_per_game
+FROM homegames as hg
+INNER JOIN teams as t
+ON hg.team = t.teamid
+
+WHERE yearid = 2016
+	AND games >= 10
+GROUP BY t.name, hg.park
+ORDER BY avg_attendance_per_game desc
+LIMIT 5
+)
+	
+UNION ALL
+
+(
+SELECT 
+	hg.park,
+	t.name,
+	SUM(hg.attendance) / SUM(hg.games) AS avg_attendance_per_game
+FROM homegames as hg
+INNER JOIN teams as t
+ON hg.team = t.teamid
+
+WHERE yearid = 2016
+	AND games >= 10
+GROUP BY t.name, hg.park
+ORDER BY avg_attendance_per_game
+LIMIT 5
+)
+
+************************************************************************************************
+
+-- 9. Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? Give their full name and the teams that they were managing when they won the award.
+
+
+
+select teamid, name, playerid
+from managers
+inner join teams
+using (teamid)
+
+
+
+
+
 
 
 
